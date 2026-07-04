bc076605fcd3c114ac664183cfa3c55b8e09b11a
Machila <Machila_1029@outlook.com>
2026-07-05 00:58:01 +0800

    feat: add new SQL queries

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE employers (
     company_name VARCHAR(250),
     company_location VARCHAR(300),
-    -- yearly_revenue FLOAT(5,2),   --Approximation of yearly revenue, but not precise enough for financial data
+
     yearly_revenus NUMERIC(10, 2),  --Exact precision for financial data, suitable for storing monetary values
-     is_hiring BOOLEAN
+    is_hiring BOOLEAN
 );
---
+++ 07-insert-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employers (company_name, company_location, yearly_revenue, is_hiring)
+VALUES ('Learning Inc', 'Educationstreet 12, London', 0.87, TRUE);
+
+INSERT INTO employers (company_name, company_location, yearly_revenue, is_hiring)
+VALUES ('Big Oil Inc', 'Clipperystreet 110, Houston', 112.55, FALSE);
+
+INSERT INTO employers (company_name, company_location, yearly_revenue, is_hiring)
+VALUES ('Hipster Food', 'Avocadstreet 5, Berlin', 6.12, TRUE);
